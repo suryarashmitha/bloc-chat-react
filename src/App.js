feb7101e18eeae38503a5d4c2567adb080d3be76
RD <rd@Suryas-MacBook-Pro.local>
2018-07-10 11:14:28 -0500

    assignment-listchatrooms:error/warning with key

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+import RoomList from './components/RoomList';
 
+var config = {
+    apiKey: "AIzaSyCnmwUQ26TDyyid1pmD0bnXTvRJF2hZOgw",
+    authDomain: "bloc-chat-99b69.firebaseapp.com",
+    databaseURL: "https://bloc-chat-99b69.firebaseio.com",
+    projectId: "bloc-chat-99b69",
+    storageBucket: "bloc-chat-99b69.appspot.com",
+    messagingSenderId: "676230122416"
+  };
+  firebase.initializeApp(config);
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RoomList firebase={firebase} />
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      rooms: []
+    }
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+   }
+
+   componentDidMount() {
+     this.roomsRef.on('child_added', snapshot => {
+       var obj = {};
+       const room = {};
+       obj.room = snapshot.val();
+       obj.key = snapshot.key;
+       this.setState({ rooms: this.state.rooms.concat( obj.room ) });
+     });
+   }
+
+    render(){
+      return(
+
+        <section className='room-list'>
+        <ul>
+          {
+         this.state.rooms.map( (room, index) =>
+           <li key={index} >
+              {room.name}
+           </li>
+         )
+       }
+       </ul>
+      </section>
+      );
+  }
+}
+
+export default RoomList;
