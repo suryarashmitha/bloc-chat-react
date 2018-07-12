eb80cf71d249dc543cce06205757a345ffe7ac6a
RD <rd@Suryas-MacBook-Pro.local>
2018-07-12 10:04:35 -0500

    error with obj in roomslist comp

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -17,19 +17,43 @@ class RoomList extends Component {
        this.setState({ rooms: this.state.rooms.concat( obj ) })
      });
    }
+   createRoom(e) {
+     e.preventDefault();
+     if(!this.state.newRoomName){
+       return
+     }
+     else {
+     this.roomsRef.push( {name: this.state.newRoomName});
+       this.setState( {newRoomName: ""} )
+     }
+   }
+   handleChange(e){
+     this.setState(
+       {newRoomName: e.target.value}
+     );
+   }
 
     render(){
       return(
+
         <section className='room-list'>
-        <ul>
           {
          this.state.rooms.map( (room, index) =>
-           <li key={index} >
+           <div className="rooms" key={index}
+                onClick={() => this.setActiveRoom(room)} >
               {room.room}
-           </li>
+           </div>
          )
        }
-       </ul>
+       <div id="create-room">
+        <form onSubmit={ (e) => this.createRoom(e) }>
+         <input type="text"
+                value={this.state.newRoomName}
+                placeholder="Enter a new room name."
+                onChange= { (e) => this.handleChange(e) } />
+         <input type="submit" value="Create New Room"/>
+        </form>
+        </div>
       </section>
       );
   }
